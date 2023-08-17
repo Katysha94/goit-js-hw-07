dd9e8963ffe1c8ee757319177655afe73c69d2e3
Katysha94 <Vasilevakata76@gmail.com>
2023-08-17 17:18:47 +0300

    commit1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery")
+
+const renderList = (arr) => arr.map(({ original, preview, description }) =>
+ `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+<img
+class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"
+/>
+</a>
+</li>`).join("");
+
+
+galleryList.insertAdjacentHTML("beforeend", renderList(galleryItems));
+
+// galleryList.addEventListener('click', )
